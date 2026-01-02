109d69ede679b0967b7b5115e4294780687ddf34
Irengo <freddricktropico14@gmail.com>
2026-01-02 16:14:58 +0800

    Done Setting Up Requirements - Ready To Code

---
+++ activity7-tms-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="pt-10 text-right text-green-400 text-2xl pr-10">  Tailwind Working ! </div>
+    </div>
+  );
+} 
+
+export default App;
